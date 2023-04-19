bc6032a90c928a6888b4ac951738f082f2bc1e0e
Titus Tran (TECHVIFY.G1) <titus.tran@techvify.com.vn>
2023-04-19 08:31:22 +0000

    [DataClaws] Auto delete old data before X days from today

--- modules/clb-service/src/main/java/com/tvf/clb/service/repository/MeetingSiteRepository.java
+++ modules/clb-service/src/main/java/com/tvf/clb/service/repository/MeetingSiteRepository.java
@@ -1,10 +1,14 @@
 package com.tvf.clb.service.repository;
 
 import com.tvf.clb.base.entity.MeetingSite;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+import java.time.Instant;
 import java.util.List;
 
 @Repository
@@ -12,5 +16,8 @@ public interface MeetingSiteRepository extends R2dbcRepository<MeetingSite, Long
     Flux<MeetingSite> findAllByMeetingSiteIdInAndSiteId(List<String> meetingIds, Integer siteId);
 
     Flux<MeetingSite> findAllByMeetingSiteIdIn(List<String> meetingSiteId);
+
+    @Query("delete from clb_db.meeting_site m where m.start_date between :startTime and :endTime")
+    Mono<Long> deleteAllByStartDateBetween(@Param("startTime") Instant startTime, @Param("endTime") Instant endTime);
 
 }
