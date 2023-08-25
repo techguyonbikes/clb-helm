bf134a1059734830d3c6399943d370c1c056f45d
Hughie Nguyen <tvf-hughie.nguyen@tech.jago.com>
2023-08-25 15:13:18 +0700

    Beans debugging

--- modules/clb-service/src/main/java/com/tvf/clb/service/service/ServiceLookup.java
+++ modules/clb-service/src/main/java/com/tvf/clb/service/service/ServiceLookup.java
@@ -1,14 +1,19 @@
 package com.tvf.clb.service.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tvf.clb.base.anotation.ClbService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.Map;
 
 @Component
+@Slf4j
 public class ServiceLookup {
 
     @Autowired
@@ -33,5 +38,19 @@ public class ServiceLookup {
 
     public <T> T forBean(Class<T> serviceClass) {
         return applicationContext.getBean(serviceClass);
+    }
+
+    @PostConstruct
+    void showBeans(){
+        Map<String, ICrawlService> beans = applicationContext.getBeansOfType(ICrawlService.class);
+        log.info("LIST BEANS OF TYPE : ICrawlService");
+        beans.forEach((k,v) -> {
+            log.info(k);
+            try {
+                log.info(new ObjectMapper().writeValueAsString(v));
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 }
